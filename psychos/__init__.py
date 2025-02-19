dd4a6a3501f3922c1da5614bea1bb6e8cbfd9136
pablomm <pablomm@users.noreply.github.com>
2025-02-19 17:16:07 +0100

    Module for connecting to serial and parallel ports and send triggers

--- psychos/__init__.py
+++ psychos/__init__.py
@@ -13,22 +13,21 @@ from .__version__ import __version__
 from .utils.lazy import attach
 
 
-submodules = ["visual", "utils", "core", "gui"]
+submodules = ["visual", "utils", "core", "gui", "triggers"]
 submod_attrs = {
     "visual": ["Window", "get_window", "Text", "Image"],
 }
 
-__getattr__, __dir__, __all__ = attach(
-    __name__, submodules=submodules, submod_attrs=submod_attrs
-)
+__getattr__, __dir__, __all__ = attach(__name__, submodules=submodules, submod_attrs=submod_attrs)
 
 __all__ += ["__version__"]
 
 if TYPE_CHECKING:
-    __all__ = ["__version__", "visual", "utils", "core", "Window", "Text", "Image"]
+    __all__ = ["__version__", "visual", "utils", "core", "triggers", "Window", "Text", "Image"]
 
     from . import visual
     from . import utils
     from . import core
+    from . import triggers
 
     from .visual import Window, Text, Image
